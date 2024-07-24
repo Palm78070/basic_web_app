6e4315d6154fd522ade09788056118762afb3d31
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2024-07-25 03:22:01 +0700

    add(frontend): Create chat-room in userPage.html pending(chat): Chat request, websocket connection, javascript

---
+++ srcs/static/js/chat.js
@@ -0,0 +1,8 @@
+// document.getElementById("chat-form").addEventListener("submit", function(event) {
+// 	event.preventDefault();
+
+// })
+document.getElementById("submit").addEventListener("click", function() {
+	const message = document.getElementById("chat-input").value
+	console.log(message)
+})
