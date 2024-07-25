99fda62c493ffb79c23ee2314c28116b8e48ae64
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2024-07-26 00:00:58 +0700

    add(chat frontend): Add websocket connection in chat.js
    Be able to connect to websocket when acces in userPage

--- srcs/static/js/chat.js
+++ srcs/static/js/chat.js
@@ -1,8 +1,28 @@
-// document.getElementById("chat-form").addEventListener("submit", function(event) {
-// 	event.preventDefault();
 
-// })
+const login_username = (function() {
+	return document.getElementById("login-username").innerText.split(" ")[2];
+})();
+console.log("login_user: ", login_username);
+
+const recipient = document.getElementById("username").innerText
+console.log("recipient: ", recipient)
+
+chat_socket = new WebSocket(
+	'ws://' +
+	window.location.host +
+	'/ws/private/' +
+	login_username
+);
+
+chat_socket.addEventListener('open', function(e){
+	console.log("Websocket is now open")
+})
+
 document.getElementById("submit").addEventListener("click", function() {
-	const message = document.getElementById("chat-input").value
-	console.log(message)
+	const message = document.getElementById("chat-input").value;
+	if (message === "") {
+		return;
+	}
+	console.log(message);
+	document.getElementById("chat-input").value = "";
 })
